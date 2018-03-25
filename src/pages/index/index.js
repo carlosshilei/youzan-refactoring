2b6206c008968ae242599bea481bdd01778e3f55
Carlos <carlos.shilei@gmail.com>
2018-03-25 22:22:21 +0800

    footer component

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,18 +3,46 @@ import './index.css'
 import url from 'js/api.js'
 import Vue from 'vue';
 import axios from 'axios'
+import { InfiniteScroll } from 'mint-ui'
+import Foot from 'components/footer.vue'
+
+Vue.use(InfiniteScroll);
 
 let app = new Vue({
   el: "#app",
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false
   },
   created() {
-    axios.get(url.hotLists,{
-      pageNum: 1,
-      pageSize: 6
-    }).then(res => {
-      this.lists = res.data.lists
-    })
+    this.getLists()
+  },
+  methods: {
+    getLists() {
+      if(this.allLoaded) return
+      this.loading = true
+      axios.get(url.hotLists,{
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res => {
+        let currentLists = res.data.lists
+        if(currentLists.length < this.pageSize) {
+          this.allLoaded = true
+        }
+        if(this.lists) {
+          this.lists = this.lists.concat(currentLists)
+        } else {
+          this.lists = currentLists
+        }
+        this.loading = false
+        this.pageNum++
+      })
+    }
+  },
+  components: {
+    Foot
   }
 })
